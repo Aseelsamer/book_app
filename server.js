a31734b3782053bb2779a7aee7c36f8549427d6d
Aseelsamer <assoul_08@hotmail.com>
2020-11-29 14:54:47 +0200

    merging

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+
+
+const express = require('express');
+require('dotenv').config();
+const server = express();
+const pg = require('pg');
+const client  = new pg.Client(process.env.DATABASE_URL);
+const superagent = require('superagent');
+const cors = require('cors');
+server.use(cors());
+
+const PORT = process.env.PORT;
+
+server.use(express.static('./public'));
+server.set('view engine','ejs');
+
+server.get('/',(req,res)=>{
+  res.render('pages/index');
+});
+
+server.get('*', (req, res) => {
+  res.status(400).send('Not found');
+});
+server.use((error, req, res) => {
+  res.status(500).send('Sorry, something went wrong');
+});
+
+
+client.connect()
+  .then(() => {
+    server.listen(PORT, ()=>{
+      console.log(`Listening on port ${PORT}`);
+    });
+  });
